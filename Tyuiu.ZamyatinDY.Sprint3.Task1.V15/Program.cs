6a56b9a1ce304f9e785b91d88aeb751dbf1dad56
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-10-22 22:33:21 +0500

    Task1

---
+++ Tyuiu.ZamyatinDY.Sprint3.Task1.V15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZamyatinDY.Sprint3.Task1.V15.Lib;
+namespace Tyuiu.ZamyatinDY.Sprint3.Task1.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds= new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Замятин Д. Ю. | ИИПб 23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПб 23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Написать программу используя цикл while, которая вычисляет     *");
+            Console.WriteLine("* произведение ряда по формуле, при х=2                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 2;
+            int startValue = 1;
+            int stopValue = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value,startValue,stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
